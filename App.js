70a984ed36a217ba8e26a79d428e0aff2f7f5ae1
SusumanthSamay <137556532+SusumanthSamay@users.noreply.github.com>
2024-03-19 14:23:42 +0530

    src

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          GitHub Codespaces <span className="heart">♥️</span> React
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './style/App.css';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
